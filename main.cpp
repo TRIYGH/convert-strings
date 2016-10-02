ae029a1c22ee7df9ab83c44dcd3d64ef96ce9264
T. Robert Ward <trironyard@icloud.com>
2016-10-02 18:24:31 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "***** Convert Strings *****\n" << endl;
+
+    string time;
+    int numChars, timeNum;
+
+    cout << "Enter in the time in the following format - H:M:S  ";
+    cin >> timeNum;
+
+    numChars = time.length();
+
+    string textTTT = "10";
+
+    timeNum = strtod(textTTT);
+
+    cout << text;
+
+    return 0;
+}
